61aad9580860e58832c901ce549028282b874990
Justin Nelson <justin.john.nelson@gmail.com>
2021-05-15 11:15:16 -0500

    fleshing out blog design

--- src/jjnguy.com/wwwroot/app/src/App.svelte
+++ src/jjnguy.com/wwwroot/app/src/App.svelte
@@ -1,8 +1,11 @@
 <script>
 	import Post from "./Post.svelte";
 	import { currentView, navigate } from "./router";
+	import TagList from "./TagList.svelte";
 
 	let posts = { loading: true };
+	let tags = { loading: true };
+	let authors = { loading: true };
 
 	fetch(
 		//`https://localhost:7001/api/v1/public/collections/h7D1FLVTcUON42qBpoLIVg/data`,
@@ -15,12 +18,36 @@
 	)
 		.then((resp) => resp.json())
 		.then((json) => (posts = json));
+
+	fetch(
+		//`https://localhost:7001/api/v1/public/collections/h7D1FLVTcUON42qBpoLIVg/data`,
+		`https://tegrity-content.azurewebsites.net/api/v1/public/collections/dq1E1EbsHUSIva8ZAfCrYg/data`,
+		{
+			headers: {
+				"x-api-key": "fmoRmTVnCUKEjD_FY__enQ",
+			},
+		}
+	)
+		.then((resp) => resp.json())
+		.then((json) => (tags = json));
+
+	fetch(
+		//`https://localhost:7001/api/v1/public/collections/h7D1FLVTcUON42qBpoLIVg/data`,
+		`https://tegrity-content.azurewebsites.net/api/v1/public/collections/_MiiHrbqKkWAp5nR3RLtHw/data`,
+		{
+			headers: {
+				"x-api-key": "fmoRmTVnCUKEjD_FY__enQ",
+			},
+		}
+	)
+		.then((resp) => resp.json())
+		.then((json) => (authors = json));
 </script>
 
 <main>
 	{#if $currentView.viewName == "home"}
 		<h1>THE jjnguy Blog</h1>
-		{#if !posts.loading}
+		{#if !posts.loading && !tags.loading && !authors.loading}
 			<ol>
 				{#each posts as post}
 					<li>
@@ -30,7 +57,18 @@
 								href={`/posts/${post.id}`}>{post.data.Title}</a
 							>
 						</h2>
-						<p>{post.data.Description}</p>
+						<p class="authors">
+							By -
+							{#each post.data.Authors.map((postAuthor) => authors.filter((a) => a.id == postAuthor)[0]) as author}
+								{author.data.Name}
+							{/each}
+						</p>
+						<p class="description">{post.data.Description}</p>
+						<TagList
+							tags={post.data.Tags?.map(
+								(tId) => tags.filter((t) => t.id == tId)[0]
+							)}
+						/>
 					</li>
 				{/each}
 			</ol>
@@ -44,23 +82,16 @@
 		{$currentView.viewName}
 	{/if}
 </main>
+<footer>
+	<a target="_blank" href="https://github.com/jjnguy">&copy; 2021 - jjnguy</a>
+</footer>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
 		max-width: 240px;
-		margin: 0 auto;
 	}
 
 	h1 {
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		font-size: 2em;
 	}
 </style>
---
+++ src/jjnguy.com/wwwroot/app/src/TagList.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let tags;
+
+  console.log(tags);
+</script>
+
+{#if tags}
+  <div class="tag-list">
+    {#each tags as tag}
+      <span class="tag">{tag.data.value}</span>
+    {/each}
+  </div>
+{/if}
+
+<style>
+  .tag {
+    border: 1px solid black;
+    padding: 0.3rem;
+  }
+</style>
