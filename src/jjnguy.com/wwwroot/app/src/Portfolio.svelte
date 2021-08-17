f01bc511bab8874f690f4622ef51e578143c6641
Justin Nelson <justin.john.nelson@gmail.com>
2021-08-16 20:26:47 -0500

    styling for portfolio page

--- src/jjnguy.com/wwwroot/app/src/Portfolio.svelte
+++ src/jjnguy.com/wwwroot/app/src/Portfolio.svelte
@@ -1,11 +1,15 @@
 <script>
-  import { dataset_dev } from "svelte/internal";
   import { getData } from "./DataAccess";
 
   let portfolio = { loading: true };
   getData(`/api/v1/public/computed-collections/nGziMur56EuuxpMNVIGmNg/data`)
     .then((resp) => resp.json())
-    .then((json) => (portfolio = json));
+    .then(
+      (json) =>
+        (portfolio = json.sort((one, two) => {
+          return one.data.Name.localeCompare(two.data.Name);
+        }))
+    );
 </script>
 
 {#if portfolio.loading}
@@ -14,28 +18,56 @@
   {#each portfolio as item}
     <section>
       <h2>{item.data.Name}</h2>
-      {#if item.data.image}
+      <div class="container">
+        {#if item.data.image}
+          <div>
+            <img
+              width="300"
+              src={JSON.parse(item.data.image?.data ?? "{}")?.url}
+              alt={item.data.image.alt}
+            />
+          </div>
+        {/if}
         <div>
-          <img
-            width="300"
-            src={JSON.parse(item.data.image?.data ?? "{}")?.url}
-            alt={item.data.image.alt}
-          />
+          <p>
+            {item.data.description}
+          </p>
+          <a href={item.data["primary link"]} target="_blank"
+            >Check it out! &#8594;</a
+          >
         </div>
-      {/if}
-      <div>
-        {item.data.description}
-      </div>
-      <div>
-        <a href={item.data["primary link"]} target="_blank">Check it out!</a>
       </div>
     </section>
   {/each}
 {/if}
 
 <style lang="less">
+  h2 {
+    margin-bottom: 1rem;
+  }
+
   section {
     margin-left: 0.5rem;
     margin-right: 0.5rem;
+
+    margin-top: 2rem;
+  }
+
+  .container {
+    display: flex;
+
+    p {
+      margin-bottom: 1rem;
+    }
+
+    div {
+      flex: 1;
+    }
+
+    a {
+      font-size: 1.2rem;
+      padding: 0.5rem 1rem;
+      border: 1px solid black;
+    }
   }
 </style>
