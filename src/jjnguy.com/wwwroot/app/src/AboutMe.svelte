3c14791ecc8191f7245bd2704d221bbd1bc7e96c
Justin Nelson <justin.john.nelson@gmail.com>
2021-08-16 21:05:29 -0500

    about me enhancements

--- src/jjnguy.com/wwwroot/app/src/AboutMe.svelte
+++ src/jjnguy.com/wwwroot/app/src/AboutMe.svelte
@@ -1,1 +1,65 @@
-Hi - my name is Justin. I'm a software engineer from Minneapolis, Minnesota.
+<img src="/img/Profile.jpg" alt="jjnguy's profile pic" />
+<p>
+  Hi - my name is Justin. I'm a software engineer from Minneapolis, Minnesota.
+</p>
+<p>
+  I have a passion for building cool things and solving complex problems. My
+  skillset and experience is broad. And I welcome new challenges.
+</p>
+<p>I consider myself an expert in the following:</p>
+<ul class="skill-list">
+  <li>C#</li>
+  <li>Javascript</li>
+  <li>CSS</li>
+  <li>SQL</li>
+  <li>Java</li>
+  <li>.Net</li>
+  <li>Node</li>
+  <li>MSSQL</li>
+  <li>PostgreSQL</li>
+  <li>NoSQL</li>
+  <li>Azure</li>
+  <li>Event-Driven Design</li>
+  <li>Cloud Architecture</li>
+</ul>
+<p>If you want to get to know me better - check me out on various platforms:</p>
+<ul>
+  <li>
+    <a target="_blank" href="https://www.linkedin.com/in/justin-nelson-870739b/"
+      >Linked-In</a
+    >
+  </li>
+  <li><a target="_blank" href="https://github.com/jjnguy">GitHub</a></li>
+  <li>
+    <a target="_blank" href="https://stackoverflow.com/users/2598/jjnguy"
+      >Stack Overflow</a
+    >
+  </li>
+</ul>
+
+<style lang="less">
+  img {
+    width: 100%;
+  }
+
+  p,
+  ul {
+    font-size: 1.2rem;
+    margin-bottom: 1rem;
+  }
+
+  ul li {
+    font-size: 1.3rem;
+  }
+
+  ul.skill-list {
+    display: flex;
+    flex-direction: column;
+    max-height: 8rem;
+    flex-wrap: wrap;
+
+    li {
+      flex: 1;
+    }
+  }
+</style>
